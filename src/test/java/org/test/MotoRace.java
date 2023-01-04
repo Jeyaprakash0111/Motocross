ba2bd95308a545de481dff205b906fa2d66c8cd6
Jeyaprakash0111 <jeyaprakashgrace@gmail.com>
2023-01-04 19:35:16 +0530

    default methods added

---
+++ src/test/java/org/test/MotoRace.java
@@ -0,0 +1,18 @@
+package org.test;
+
+public class MotoRace {
+	
+	private void ktm() {
+		System.out.println("ktm");
+	}
+	private void royalEnfield() {
+		System.out.println("enfield");
+	}
+	private void kawasaki() {
+		System.out.println("kawasaki");
+	}
+	private void yamaha() {
+		System.out.println("yamaha");
+	}
+
+}
